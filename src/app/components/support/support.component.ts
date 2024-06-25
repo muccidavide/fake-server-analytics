706fdcc4a28ee9d6f67d18463c1dd88d5f8e7eb2
Davide Mucci <davidemucci0804@gmail.com>
2024-06-25 18:04:20 +0200

    partial commit

--- src/app/components/support/support.component.ts
+++ src/app/components/support/support.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
-  selector: 'app-support',
-  standalone: true,
-  imports: [],
-  templateUrl: './support.component.html',
-  styleUrl: './support.component.css'
+    selector: 'app-support',
+    standalone: true,
+    templateUrl: './support.component.html',
+    styleUrl: './support.component.css',
+    imports: [NewTicketComponent]
 })
 export class SupportComponent {
 
