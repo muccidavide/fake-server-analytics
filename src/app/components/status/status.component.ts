b7a59021bd0c566b943828617e35538596521fcc
Davide Mucci <davidemucci0804@gmail.com>
2024-06-26 17:50:17 +0200

    partial

--- src/app/components/status/status.component.ts
+++ src/app/components/status/status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnDestroy, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-status',
@@ -7,7 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './status.component.html',
   styleUrl: './status.component.css'
 })
-export class StatusComponent {
-  currentStatus = 'online';
+export class StatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+   const interval =  setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.99999
+      if(rnd < 0.5){
+        this.currentStatus = 'online'
+      }else if(rnd < 0.9){
+        this.currentStatus = 'offline'
+      }else{       
+        this.currentStatus = 'unknown'
+      }
+    }, 5000)
+    this.destroyRef.onDestroy(()=> {
+      clearInterval(interval);
+    })
+
+  }
 
 }
--- src/app/components/support/new-ticket/new-ticket.component.ts
+++ src/app/components/support/new-ticket/new-ticket.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { ControlComponent } from "../../../shared/control/control.component";
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-ticket',
-  standalone: true,
-  imports: [],
-  templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+    selector: 'app-new-ticket',
+    standalone: true,
+    templateUrl: './new-ticket.component.html',
+    styleUrl: './new-ticket.component.css',
+    imports: [ButtonComponent, ControlComponent, FormsModule]
 })
 export class NewTicketComponent {
-
+    //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+    private form = viewChild.required<ElementRef<HTMLFormElement>>('form'); 
+    onSubmit(title: string, text:string){
+        console.log(title, text);
+        this.form().nativeElement.reset();
+        
+    }
 }
